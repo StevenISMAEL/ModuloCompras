f5e26c2ffe3134e7049030a31286ff70a694a84e
JostinQuilca <jdquilcap1@utn.edu.ec>
2025-06-10 20:37:29 -0500

    Agregado con Swagger

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const { default: serverlessExpress } = require('@codegenie/serverless-express');
-const app = require('../src/app'); // Reutilizamos todo tu código
+const serverless = require('serverless-http');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('../src/docs/swaggerConfig');
 
-module.exports = serverlessExpress({ app });
+const app = require('../src/app');
+
+// (1) Asegura que la ruta esté activa desde aquí también
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+module.exports = serverless(app);
